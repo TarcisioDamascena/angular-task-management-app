6c022c8d061cb7ffa33fc4d2afdec0ca0df2d1cd
DamasX <84998412+TarcisioDamascena@users.noreply.github.com>
2025-01-23 12:32:43 -0300

    Feat: create auth interceptor and guard

---
+++ src/app/guards/auth.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { map, Observable } from "rxjs";
+import { Router ,UrlTree } from "@angular/router";
+
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AuthGuard {
+
+    constructor(private authService: AuthService, private router: Router) { }
+
+    canActivate(): Observable<boolean | UrlTree> {
+        return this.authService.isAuthenticated$.pipe(
+            map(isAuthenticated => isAuthenticated || this.router.createUrlTree(['/login']))
+        )
+    }
+}
